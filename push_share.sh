dcc273452cab750dfbf87e0e20580b3bc7d7621a
Bardia Jedi <bardiajedi@gmail.com>
2021-10-19 12:35:47 +0200

    added feature to share script

--- push_share.sh
+++ push_share.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
 
 mkdir -p /usr/share/wallpapers/bardia/
+mkdir -p /usr/share/icons/bardia/
+mkdir -p /usr/share/wallpapers/bardia/lockscreen
+
 cp ./wallpapers/* /usr/share/wallpapers/bardia/
+cp ./icons/* /usr/share/icons/bardia/
+
+
+## create i3lock compatible image
+filename='ink_in_water'
+convert -resize $(xdpyinfo | grep dimensions | cut -d\  -f7 | cut -dx -f1) ./wallpapers/$filename.jpg /usr/share/wallpapers/bardia/lockscreen/$filename.png
