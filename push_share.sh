2ddf757f87a6c82976bbba92c4a024586913415c
Bardia Jedi <bardiajedi@gmail.com>
2021-11-21 11:11:35 +0100

    added sudo

--- push_share.sh
+++ push_share.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-mkdir -p /usr/share/wallpapers/bardia/
-mkdir -p /usr/share/icons/bardia/
-mkdir -p /usr/share/wallpapers/bardia/lockscreen
+sudo mkdir -p /usr/share/wallpapers/bardia/
+sudo mkdir -p /usr/share/icons/bardia/
+sudo mkdir -p /usr/share/wallpapers/bardia/lockscreen
 
-cp ./wallpapers/* /usr/share/wallpapers/bardia/
-cp ./icons/* /usr/share/icons/bardia/
+sudo cp ./wallpapers/* /usr/share/wallpapers/bardia/
+sudo cp ./icons/* /usr/share/icons/bardia/
 
 
 ## create i3lock compatible image
 filename='ink_in_water'
-convert -resize $(xdpyinfo | grep dimensions | cut -d\  -f7 | cut -dx -f1) ./wallpapers/$filename.jpg /usr/share/wallpapers/bardia/lockscreen/$filename.png
+sudo convert -resize $(xdpyinfo | grep dimensions | cut -d\  -f7 | cut -dx -f1) ./wallpapers/$filename.jpg /usr/share/wallpapers/bardia/lockscreen/$filename.png
