26ad93fdbf40f0b3202d4ab362e33efd4e8d83ce
Bardia Jedi <bardiajedi@gmail.com>
2021-11-23 22:17:30 +0100

    changed toggle picom

--- qtile/scripts/picom-toggle.sh
+++ qtile/scripts/picom-toggle.sh
@@ -3,5 +3,5 @@ if pgrep -x "picom" > /dev/null
 then
 	killall picom
 else
-	picom -b --config ~/.config/picom/picom-blur.conf
+	picom -b --experimental-backends #--config #~/.config/picom/picom-blur.conf
 fi
