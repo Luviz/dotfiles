3ee1dc6096b1c7d33cade06c2d914eac8e7de761
Bardia Jedi <bardiajedi@gmail.com>
2021-11-30 19:38:51 +0100

    added fish config

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+neofetch
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias ls="exa -l"
+alias la="exa -la"
+alias lt="exa -lT"
+alias cat="bat"
+
+alias code="code-insiders"
+
+starship init fish | source
