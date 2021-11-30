d9298d13e632632cda5c8a7d422a37cc1a2f020b
Bardia Jedi <bardiajedi@gmail.com>
2021-11-30 19:23:53 +0100

    added alacritty to pull/push

--- pull.sh
+++ pull.sh
@@ -7,3 +7,7 @@ cp -rf ~/.config/starship.toml ./starship/
 ## qtile
 mkdir -p ./qtile
 cp -rf ~/.config/qtile .
+
+## alacritty
+mkdir -p ./alacritty
+cp -rf ~/.config/alacritty/alacritty.yml ./alacritty/alacritty.yml
--- push.sh
+++ push.sh
@@ -8,3 +8,7 @@ cp ./starship/starship.toml ~/.config/starship.toml
 
 ## qtile
 cp -r ./qtile/* ~/.config/qtile/
+
+## alacritty
+mkdir -p ~/.config/alacritty/
+cp ./alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
