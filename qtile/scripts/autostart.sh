2bf3ded3e7256c09a48ac179b4b9b872164d0f26
Bardia Jedi <bardiajedi@gmail.com>
2024-03-05 18:30:34 +0100

    chore(qtile): spacing in auto start script

--- qtile/scripts/autostart.sh
+++ qtile/scripts/autostart.sh
@@ -10,35 +10,35 @@ function run {
 # wired -r &
 systemctl --user start wired_notify.service &
 
-#starting utility applications at boot time
+# starting utility applications at boot time
 lxsession &
 run nm-applet &
 # run pamac-tray &
 numlockx on &
 run blueman-applet &
-#flameshot &
-#picom --config $HOME/.config/picom/picom.conf &
+# flameshot &
+# picom --config $HOME/.config/picom/picom.conf &
 picom -b --backend glx &
-#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+# /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 # dunst &
 feh --randomize --bg-fill /usr/share/wallpapers/bardia/*
-#starting user applications at boot time
+# starting user applications at boot time
 # run pasystray &
 run cbatticon &
-#run teams &
-run slack
+# run teams &
+# run slack &
 run tuxedo-control-center &
-#run prospect-mail &
-#run discord &
-#run caffeine -a &
-#run vivaldi-stable &
-#run firefox &
-#run thunar &
-#run dropbox &
-#run insync start &
-#run spotify &
-#run atom &
-#run telegram-desktop &
+# run prospect-mail &
+# run discord &
+# run caffeine -a &
+# run vivaldi-stable &
+# run firefox &
+# run thunar &
+# run dropbox &
+# run insync start &
+# run spotify &
+# run atom &
+# run telegram-desktop &
 
 ## Autolocj screen
 xss-lock -- i3lock -efi /usr/share/wallpapers/bardia/lockscreen/*.png &
