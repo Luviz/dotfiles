9851a2c0f08bae65bd6faf45794c83709072e288
Bardia Jedi <bardiajedi@gmail.com>
2024-03-07 13:58:59 +0100

    feat(nvim): added ctrl esc to exit when in terminal

--- nvim/lua/bardia/keymaps.lua
+++ nvim/lua/bardia/keymaps.lua
@@ -61,6 +61,8 @@ keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", default_opt)
 
 -- Terminal --
 -- Better terminal navigation
+keymap("t", "<C-ESC>", "<C-\\><C-N>", term_opts)
+
 keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
